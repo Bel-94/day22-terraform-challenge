187ab68a3fb13752e00f627ef4ac247efeea553f
Bel-94 <ntinyaribelinda@gmail.com>
2026-04-09 10:44:45 +0300

    day 22: integrated CI pipeline, Sentinel policies, cost gate

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 
   # Remote state with locking — Terraform Cloud workspace
   backend "remote" {
-    organization = "Bel_terr_acc"
+    organization = "Bel_terra_acc"
     workspaces {
       name = "day22-integrated"
     }
